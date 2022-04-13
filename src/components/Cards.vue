e1f7e598b5e260f284c1b05bf3e2929f56ee4df2
Frank <frankjoe261@gmail.com>
2022-04-13 07:37:35 +0100

    just consumed api to show image list

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,55 +1,40 @@
 <template>
-  <b-container fluid>
-    <b-row>
-      <b-col
-        lg="3"
-        md="4"
-        sm="12"
-        v-for="card in cards"
-        :card="card"
-        :key="card.id"
+  <div class="container-fluid">
+    <div class="row">
+      <div
+        class="col-lg-3 col-md-4 col-sm-12 col-12"
+        v-for="(card, index) in cards"
+        v-bind:key="index"
       >
-        <b-card title="card.name" img-src="card.img" img-alt="image" img-top>
-          <b-button href="#" variant="primary">View More</b-button>
-        </b-card>
-      </b-col>
-    </b-row>
-  </b-container>
+        <div class="card col-12 p-0 my-1">
+          <img :src="card" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary mx-auto">View More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "card",
   data() {
-    return {
-      cards: [
-        {
-          id: 1,
-          name: "James",
-          handle: "@jokerjames",
-          img: "https://semantic-ui.com/images/avatar2/large/matthew.png",
-          tweet: "If you don't succeed, dust yourself off and try again.",
-          likes: 10,
-        },
-        {
-          id: 2,
-          name: "Fatima",
-          handle: "@fantasticfatima",
-          img: "https://semantic-ui.com/images/avatar2/large/molly.png",
-          tweet: "Better late than never but never late is better.",
-          likes: 12,
-        },
-        {
-          id: 3,
-          name: "Xin",
-          handle: "@xeroxin",
-          img: "https://semantic-ui.com/images/avatar2/large/elyse.png",
-          tweet: "Beauty in the struggle, ugliness in the success.",
-          likes: 18,
-        },
-      ],
-    };
+    return { cards: undefined };
+  },
+  mounted() {
+    Vue.axios
+      .get(`https://boston-dynamics-cxhyclwxeq-uc.a.run.app/getAllFiles`)
+      .then((res) => {
+        console.log(res.data.data[0]);
+        this.cards = res.data.data;
+      });
   },
 };
 </script>
